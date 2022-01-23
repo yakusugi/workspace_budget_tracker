118adeec0efb456cafdba310fb2b2bdd0b2cc5e1
yakusugi <yosukeito1987@gmail.com>
2022-01-23 16:10:18 +0900

    commit jan 23 2022 part 2

--- BudgetTracker/src/budget_tracker/login/jUnitLoginTest.java
+++ BudgetTracker/src/budget_tracker/login/jUnitLoginTest.java
@@ -10,25 +10,17 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
 
 class jUnitLoginTest {
 
+	@BeforeAll
 	@Test
 	void loginTest() throws NamingException, SQLException {
 		InitialContext ic = new InitialContext();
-		DataSource ds = null;
-		
-		ic.createSubcontext("java:");
-		ic.createSubcontext("java:comp");
-		ic.createSubcontext("java:comp/env");
-		ic.createSubcontext("java:comp/env/jdbc");
-		
-		Context envContext  = (Context)ic.lookup("java:/comp/env");
-		DataSource dataSource;
-		dataSource = (DataSource)envContext.lookup("java:comp/env/jdbc/searchman");
-		Connection conn = null;
-		conn = ds.getConnection();
 		
 		String inputId = "testuser";
 		String inputPassword = "testpass";
@@ -36,6 +28,21 @@ class jUnitLoginTest {
 		LoginServletLogic loginServletLogic = new LoginServletLogic();
 
 		try {
+			ic.createSubcontext("java:");
+			ic.createSubcontext("java:comp");
+			ic.createSubcontext("java:comp/env");
+			ic.createSubcontext("java:comp/env/jdbc");
+			
+			MysqlDataSource ds = new MysqlDataSource();
+			ds.setUser("db_admin");
+			ds.setPassword("dbpass");
+			ds.setDatabaseName("new_budget_tracker_db");
+			ds.setServerName("localhost");
+			ds.setPortNumber(3306);
+			
+			ds = (MysqlDataSource) ic.lookup("java:comp/env/jdbc/searchman");
+
+			ic.bind("java:comp/env/jdbc/searchman", ds);
 
 			if (loginServletLogic.login(inputId, inputPassword)) {
 				System.out.println("login success");
